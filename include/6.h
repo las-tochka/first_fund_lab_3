2361c5a3835228325495edcdc2cc80aa05d37684
las-tochka <takuve@yandex.ru>
2025-09-30 12:42:38 +0300

    6 task

---
+++ include/6.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+double a(double epsilon);
+double b(double epsilon);
+double c(double epsilon);
+double d(double epsilon);
